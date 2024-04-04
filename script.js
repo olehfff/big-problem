62c867a0c38195ae037aa14cf871f541a390472e
olehfff <olegsamilenko5@gmail.com>
2024-04-04 19:24:23 +0300

    add : style.css, script.js, index.html

---
+++ script.js
@@ -0,0 +1,44 @@
+// let num = 4;
+
+// while(num <= 10) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// for(let i = 1; i <= 20; i++) {
+//     if(i % 2 === 0) {
+//         console.log(i);
+//         continue;
+//     }
+// }
+
+
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+
+// while(num <= arr.length) {
+//     console.log(arr[num]);
+//     num++;
+// }
+
+
+// for(let i = 0; i <= arr.length; i++) {
+//     console.log(arr[i])
+// }
+
+
+// let arr = [1, 2, 3, 4, 5, 6];
+// let sum = 0;
+
+// for(let i = 0; i <= arr.length; i++) {
+//     console.log(arr[i]);
+//     sum += arr[i]; sum = arr[i] + sum
+//     console.log{sum};
+// }
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let max = Math.max(...arr);
+// console.log(max)
